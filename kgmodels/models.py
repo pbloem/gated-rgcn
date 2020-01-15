Fix normalization bug

kgmodels/models.py
@@ -27,16 +27,17 @@ class RGCNClassic(nn.Module):
         _, rn = hor_size
         r = rn//n
 
-        hor_vals = torch.ones(hor_ind.size(0), dtype=torch.float)
-        hor_vals = hor_vals / util.sum_sparse(hor_ind, hor_vals, hor_size)
+        t = len(edges[0][0])
 
-        hor_graph = torch.sparse.FloatTensor(indices=hor_ind.t(), values=hor_vals, size=hor_size)
+        vals = torch.ones(ver_ind.size(0), dtype=torch.float)
+        vals = vals / util.sum_sparse(ver_ind, vals, ver_size)
+        # -- the values are the same for the horizontal and the vertically stacked adjacency matrices
+        #    so we can just normalize them by the vertically stacked one and reuse for the horizontal
+
+        hor_graph = torch.sparse.FloatTensor(indices=hor_ind.t(), values=vals, size=hor_size)
         self.register_buffer('hor_graph', hor_graph)
 
-        ver_vals = torch.ones(ver_ind.size(0), dtype=torch.float)
-        ver_vals = ver_vals / util.sum_sparse(ver_ind, ver_vals, ver_size)
-
-        ver_graph = torch.sparse.FloatTensor(indices=ver_ind.t(), values=ver_vals, size=ver_size)
+        ver_graph = torch.sparse.FloatTensor(indices=ver_ind.t(), values=vals, size=ver_size)
         self.register_buffer('ver_graph', ver_graph)
 
         # layer 1 weights

kgmodels/util/util.py
@@ -240,7 +240,7 @@ def sum_sparse(indices, values, size, row=True):
     results back to the non-sparse row/column entries
 
     Arguments are interpreted as defining sparse matrix. Any extra dimensions
-    as treated as batch.
+    are treated as batch.
 
     :return:
     """
@@ -264,15 +264,18 @@ def sum_sparse(indices, values, size, row=True):
 
     b, k, r = indices.size()
 
-    if row:
-        ones = torch.ones((size[1], 1), device=d(indices))
-    else:
-        ones = torch.ones((size[0], 1), device=d(indices))
+    if not row:
         # transpose the matrix
-        indices = torch.cat([indices[:, :, 1:2], indices[:, :, 0:1]], dim=1)
+        indices = torch.cat([indices[:, :, 1:2], indices[:, :, 0:1]], dim=2)
+        size = size[1], size[0]
+
+    ones = torch.ones((size[1], 1), device=d(indices))
 
     s, _ = ones.size()
     ones = ones[None, :, :].expand(b, s, 1).contiguous()
+
+    # print(indices.size(), values.size(), size, ones.size())
+    # sys.exit()
 
     sums = batchmm(indices, values, size, ones) # row/column sums
     bindex = torch.arange(b, device=d(indices))[:, None].expand(b, indices.size(1))